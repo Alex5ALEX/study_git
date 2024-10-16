001d3ca28e21e7695d5e1b08b3a42bef28cd5109
alex_note <sashamelnm@gmail.com>
2024-10-16 23:59:00 +0300

    ver 0.0.0.11

---
+++ pmis/lab1/app/lab_1/src/main/java/com/example/lab_1/BeerExpert.java
@@ -0,0 +1,20 @@
+package com.example.lab_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BeerExpert {
+    public BeerExpert() {
+    }
+    List<String> getBrands(String color) {
+        List<String> brands;
+        brands = new ArrayList<>();
+        if (color.equals("amber")) {
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        } else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+        return brands; }}
