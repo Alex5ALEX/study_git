9c64faf6572091d2ef45a65c67257f89b6ff92ea
alex_pc <sashamelnm@gmail.com>
2024-10-28 01:02:23 +0300

    psp start(client+server)

---
+++ PSP/lab_1/client/Client/Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        try
+        {
+            // Подключаемся к серверу
+            TcpClient client = new TcpClient("127.0.0.1", 5000);
+            NetworkStream stream = client.GetStream();
+
+            // Отправляем сообщение
+            string message = "Привет, сервер!";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine("Сообщение отправлено: " + message);
+
+            // Получаем ответ
+            byte[] buffer = new byte[256];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Ответ сервера: " + response);
+
+            // Закрываем соединение
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+}
---
+++ PSP/lab_1/server/Server/Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        // Создаем TcpListener, чтобы слушать входящие соединения
+        TcpListener listener = new TcpListener(IPAddress.Any, 5000);
+        listener.Start();
+        Console.WriteLine("Сервер запущен. Ожидание соединений...");
+
+        while (true)
+        {
+            // Принимаем входящее соединение
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Клиент подключен.");
+
+            // Получаем поток для чтения и записи
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[256];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Получено сообщение: " + message);
+
+            // Отправляем ответ
+            string response = "Сообщение получено.";
+            byte[] responseData = Encoding.UTF8.GetBytes(response);
+            stream.Write(responseData, 0, responseData.Length);
+
+            // Закрываем соединение
+            client.Close();
+        }
+    }
+}
